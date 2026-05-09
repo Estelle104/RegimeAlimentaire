f8ba7603b4c0d7688d405881f773c1bb42e27408
tendrymbolanekena <tendrymbolanekena@gmail.com>
2026-05-09 22:10:02 +0300

    perndre l imc de l itulisateur et regarder si il devrait perdre ou avoir de poid

--- app/Models/UtilisateurModel.php
+++ app/Models/UtilisateurModel.php
@@ -9,4 +9,24 @@ class UtilisateurModel extends Model
     protected $table = 'users';
     protected $primaryKey = 'id_utilisateur';
     protected $allowedFields = ['nom', 'email', 'mot_de_passe', 'genre', 'solde', 'est_gold'];
+
+    public function getImc($id_utilisateur)
+    {
+        $details = new DetailSanteModel();
+        $user = $details->find($id_utilisateur);
+        if ($user) {
+            $taille = $user['taille']; 
+            $poids = $user['poids'];   
+            $imc = $poids / (($taille) ** 2);
+
+            if($imc < 18.5) {
+                return "plus";
+            } elseif ($imc >= 18.5 && $imc < 25) {
+                return "zero";
+            } else {
+                return "moins";
+            }
+        }
+        return null;
+    }
 }
